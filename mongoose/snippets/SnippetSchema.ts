423b9668149163431440560748c15311e9172c17
osbornnick <nicholas.p.osborn@gmail.com>
2022-04-11 17:33:10 -0400

    snippet can be forkedFrom

--- mongoose/snippets/SnippetSchema.ts
+++ mongoose/snippets/SnippetSchema.ts
@@ -1,12 +1,13 @@
 import mongoose, { Schema } from "mongoose";
 import Snippet from "../../models/snippets/snippet";
 
-const SnippetSchema = new mongoose.Schema<Snippet> (
+const SnippetSchema = new mongoose.Schema<Snippet>(
     {
-        code: {type: String, required: true},
-        author: {type: Schema.Types.ObjectId, ref: "UserModel"},
-        created: {type: Date, default: Date.now},
+        code: { type: String, required: true },
+        author: { type: Schema.Types.ObjectId, ref: "UserModel" },
+        created: { type: Date, default: Date.now },
+        forkedFrom: { type: Schema.Types.ObjectId, ref: "SnippetModel" },
     },
-    {collection: "snippets"}
+    { collection: "snippets" }
 );
 export default SnippetSchema;
