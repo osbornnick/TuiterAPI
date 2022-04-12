61dce23c550110e0913738407fcc98d8aaa629d0
ghackford <hackford.j@northeastern.edu>
2022-04-11 20:27:24 -0400

    commenting out sharing for now

--- interfaces/SnippetControllerI.ts
+++ interfaces/SnippetControllerI.ts
@@ -6,5 +6,5 @@ export default interface SnippetControllerI {
     createSnippet (req: Request, res: Response): void;
     editSnippet (req: Request, res: Response): void;
     deleteSnippet (req: Request, res: Response): void;
-    shareSnippet (req: Request, res: Response): void;
+    //shareSnippet (req: Request, res: Response): void;
 }
